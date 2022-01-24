5d79e05c8e1b0b6a5abbcde7ca9fb4c283bd3890
HappyWorld <xeler.arsk@gmail.com>
2022-01-24 13:30:26 +0300

    fix bug add learning

--- app/src/main/java/com/happyworldgames/keyboard/LearningActivity.kt
+++ app/src/main/java/com/happyworldgames/keyboard/LearningActivity.kt
@@ -1,7 +1,10 @@
 package com.happyworldgames.keyboard
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 /*
     Имеется область сверху, это подсказка, так же есть область снизу, это управление, она же активная зона.
@@ -18,8 +21,38 @@ import androidx.appcompat.app.AppCompatActivity
  */
 
 class LearningActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.learning)
 
+        var learnImgPos = 0
+        val learnImgId = arrayOf(
+            R.drawable.learn_1,
+            R.drawable.learn_1_hand,
+            R.drawable.learn_2_hand,
+            R.drawable.learn_3_hand,
+            R.drawable.learn_4
+        )
+
+        findViewById<FloatingActionButton>(R.id.next_learn).setOnClickListener {
+            learnImgPos++
+            if(learnImgPos >= learnImgId.size) learnImgPos = 0
+            setImage(learnImgId[learnImgPos])
+            setText(learnImgPos)
+        }
+        findViewById<FloatingActionButton>(R.id.previous_learn).setOnClickListener {
+            learnImgPos--
+            if(learnImgPos < 0) learnImgPos = learnImgId.size - 1
+            setImage(learnImgId[learnImgPos])
+            setText(learnImgPos)
+        }
+    }
+
+    private fun setText(pos: Int) {
+        findViewById<TextView>(R.id.textView2).text = "$pos"
+    }
+    private fun setImage(id: Int) {
+        findViewById<ImageView>(R.id.imageView).setImageResource(id)
     }
 }
